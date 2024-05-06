e6165ddff0f7017d47bdd0d7026322ae32debca4
Nestor Qin <imba.qxy@gmail.com>
2024-05-06 16:07:42 -0400

    Sync version number

--- src/tel_download.js
+++ src/tel_download.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Telegram Media Downloader
 // @name:zh-CN   Telegram图片视频下载器
-// @version      1.104
+// @version      1.105
 // @namespace    https://github.com/Neet-Nestor/Telegram-Media-Downloader
 // @description  Download images, GIFs, videos, and voice messages on the Telegram webapp from private channels that disable downloading and restrict saving content
 // @description:zh-cn 从禁止下载的Telegram频道中下载图片、视频及语音消息
